bc6d0f847a1fe3eebff70e1a58bc13c2fe43b6d4
sreekanth <sreekanth@sreekanths-MacBook-Pro.local>
2018-10-01 12:21:45 +0530

    Pipline 5

--- src/main/java/1/App.java
+++ src/main/java/1/App.java
@@ -1,5 +1,3 @@
-package 1;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
